45f85d57e0f6e96afe8266b69b8b8ff04145fa98
simony0828 <simony0828@aim.com>
2022-05-27 09:26:42 -0700

    Create run_watcher.py

---
+++ run_watcher.py
@@ -0,0 +1,31 @@
+import argparse
+from lib.watcher import Watcher
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Watcher for a list of tasks/tables by reading YAML configuration file')
+
+    parser.add_argument('-f', '--file', action="store", required=True, dest='config_file',
+        help="YAML file for tables or tasks readiness check")
+    parser.add_argument('-d', '--dry_run', action="store_true", default=False, dest='dry_run',
+        help="Print all SQLs only")
+    parser.add_argument('-u', '--unit_test', action="store_true", default=False, dest='unit_test',
+        help="Enable watcher without waiting too long")
+    parser.add_argument('-v', '--variable', action="append", required=False, dest='variables', default=[],
+        help="For variables replacement")
+    return parser.parse_args()
+
+def __main__():
+    # Parse command line arguments
+    args = parse_args()
+
+    # Create Watcher object
+    w = Watcher(
+        yaml_file=args.config_file,
+        is_dry_run=args.dry_run,
+        is_unit_test=args.unit_test,
+        variables=args.variables,
+    )
+
+    w.run_watcher()
+
+__main__()
